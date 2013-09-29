f8f6ea71c92f7560fca167434d7b5084e63b9b23
hycis <hyciswu@gmail.com>
2013-09-28 21:46:33 -0400

    update

--- layers.py
+++ layers.py
@@ -80,19 +80,25 @@ class NoisyRELU(Linear):
                 desired_norms = T.clip(col_norms, 0, self.max_col_norm)
                 updates[W] = updated_W * desired_norms / (1e-7 + col_norms)
         
-        def update_bias_elemwise(b_value_for_one_neuron, active_rate):
-            if T.gt(active_rate, self.desired_active_rate):
-                rval = b_value_for_one_neuron - (active_rate - self.desired_active_rate) * self.bias_factor
-            else:
-                rval = b_value_for_one_neuron + (self.desired_active_rate - active_rate) * self.bias_factor
-            #import pdb
-            #pdb.set_trace()
-            return rval
+        
+        
+#         def update_bias_elemwise(b_value_for_one_neuron, active_rate):
+#             if T.gt(active_rate, self.desired_active_rate):
+#                 rval = b_value_for_one_neuron - (active_rate - self.desired_active_rate) * self.bias_factor
+#             else:
+#                 rval = b_value_for_one_neuron + (self.desired_active_rate - active_rate) * self.bias_factor
+#             #import pdb
+#             #pdb.set_trace()
+#             return rval
         
         assert b in updates
         
-        updates_b = updates[b]
-        updates[b] = theano.scan(update_bias_elemwise, sequences=[updates_b, self.active_rate])[0]
+#         updates_b = updates[b]
+        renormalize = (T.gt(self.desired_active_rate, self.active_rate) - 0.5) * 2
+        factor = renormalize * (self.active_rate - self.desired_active_rate) * self.bias_factor
+        updates[b] += factor
+        
+        #updates[b] = theano.scan(update_bias_elemwise, sequences=[updates_b, self.active_rate])[0]
         #import pdb
         #pdb.set_trace()
         #updates[b] = updates_b
