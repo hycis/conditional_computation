463ec3bcc2ba15f78167bff2517a27e3eccf35a6
hycis <hyciswu@gmail.com>
2013-09-28 22:38:18 -0400

    update

--- layers.py
+++ layers.py
@@ -189,9 +189,9 @@ class NoisyRELU(Linear):
         rval['active_rate_1'] = active_rate[1]
         rval['active_rate_15'] = active_rate[15]
         rval['active_rate_30'] = active_rate[30]
-        rval['active_rate_40'] = active_rate[40]
-        rval['active_rate_45'] = active_rate[45]
-        rval['active_rate_mean'] = T.sum(active_rate) / self.dim
+        rval['active_rate_max'] = T.max(active_rate)
+        rval['active_rate_min'] = T.min(active_rate)
+        rval['active_rate_mean'] = T.mean(active_rate)
  
         rval['state_shape_0'] = state.shape[0] * 1.
         rval['state_shape_1'] = state.shape[1] * 1.
