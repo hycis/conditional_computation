76aa6a0c36275d4da87c55c68a958259cbdacf47
hycis <hyciswu@gmail.com>
2013-10-03 00:55:00 -0400

    update

--- tools/default_config.py
+++ tools/default_config.py
@@ -61,7 +61,7 @@ model_config = DD({
 
             # Channel and dataset monitoring
             # mca : mean classification average of a minibatch
-            'channel_array'                 : ['fun1'],
+            'channel_array'                 : ['mca'],
             # valid or test or both
             'monitoring_dataset'           : ['test', 'valid'],
 
