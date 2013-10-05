b94eec2d59af5b647f56f58c0fb167800546f55e
hycis <hyciswu@gmail.com>
2013-10-05 01:52:10 -0400

    update

--- layers.py
+++ layers.py
@@ -14,7 +14,7 @@ class NoisyRELU(Linear):
         self.noise_factor = noise_factor
         self.adjust_threshold_factor = adjust_threshold_factor
         self.desired_active_rate = desired_active_rate
-        self.threshold = theano.shared(np.zeros(shape=(self.dim,)))
+        self.threshold = T.zeros(shape=(self.dim,), dtype=theano.config.floatX)
         #self.active_rate = theano.tensor.zeros(shape=(self.dim,), dtype=theano.config.floatX)
 
     def fprop(self, state_below):
@@ -30,8 +30,7 @@ class NoisyRELU(Linear):
         batch_size = p.shape[0]
         self.active_rate = T.gt(p, self.threshold).sum(axis=0, dtype=theano.config.floatX) / batch_size
         renormalize = (T.gt(self.active_rate, self.desired_active_rate) - 0.5) * 2
-        self.threshold = renormalize 
-        #* T.abs_(self.desired_active_rate - self.active_rate) * self.adjust_threshold_factor
+        #self.threshold = renormalize * T.abs_(self.desired_active_rate - self.active_rate) * self.adjust_threshold_factor
 #         
         return T.gt(p, self.threshold) * p
         
