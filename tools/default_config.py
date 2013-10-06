807da9f0264985578c49ee0e926dcb0de3a185b4
hycis <hyciswu@gmail.com>
2013-10-06 18:59:39 -0400

    update

--- tools/default_config.py
+++ tools/default_config.py
@@ -142,8 +142,8 @@ model_config = DD({
                     'sparse_init'           : 15,
                     'dim'                   : 2000,
                     'max_col_norm'          : 1.,
-                    'noise_factor'          : 0.01,
-                    'adjust_threshold_factor'   : 1.,
+                    'noise_factor'          : 1,
+                    'adjust_threshold_factor'   : 0.1,
                     'desired_active_rate'   : 0.1
                     }),
 
